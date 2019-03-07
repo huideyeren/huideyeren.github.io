b0eebcc22b6ce830f996e11abf8d190a02ae232a
Iosif Takakura <takakura.yusuke@gmail.com>
2019-03-08 07:57:27 +0900

    定期更新

--- config.rb
+++ config.rb
@@ -153,15 +153,15 @@ end
 # set site URL
 set :site_url, 'http://huideyeren.info'
 # set site title
-set :site_title, '恢徳堂のイオシフさんのブログ'
+set :site_title, '恢徳堂のヨーシャさんのブログ'
 # set site description (only used for meta description for the moment)
-set :site_description, 'イオシフ恢徳野人のブログです。同人活動も含む様々なことをつぶやいてます。'
+set :site_description, 'ヨーシャことイオシフ恢徳野人のブログです。IT技術のことやトミーウォーカーのPBWのこと、同人活動のことも含む様々なことをつぶやいてます。'
 # set site author name
 set :site_author, 'イオシフ恢徳野人（ヨーシャ）'
 # set site author profile information
-set :site_author_profile, '1981年、東京に産まれました。いろいろな技術に興味を持ち、一応システムエンジニアっぽいことしながら物書きもしています。発達障がい当事者です。一応正教徒です。いわゆるオタクです。「個」が尊重される、持続可能で公正な社会を目指します。 モットー：【自由・平等・寛容・生存・環境】'
+set :site_author_profile, '1981年、東京に生まれました。修士（史学）ですがいろいろな技術に興味を持ち、今ではプログラマー・システムエンジニアをしながら物書きもしています。発達障がい、とりわけADHD当事者です。MBTI診断ではENFPタイプです。一応正教会に属するクリスチャンです。いわゆるオタクです。「個」が尊重される、持続可能で公正な社会を目指します。 モットー：【自由・平等・寛容・生存・環境】'
 # set site author profile image (should be in images_dir)
-set :site_author_image, '3515381.png'
+set :site_author_image, 'mongolian.jpg'
 # when true, the page and site titles will be reversed (page title | site title)
 set :reverse_title, true
 # twitter/facebook/github/linkedin links in author page (otherwise set nil)
